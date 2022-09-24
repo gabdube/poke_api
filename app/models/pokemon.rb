90cf6de2214505998d0adddc4b5309c5cb1df8ee
Gabriel D <gdube.475@gmail.com>
2022-09-24 15:29:16 -0400

    model tests & extra type validation

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -14,10 +14,14 @@
 # t.integer :generation
 # t.boolean :legendary
 class Pokemon < ApplicationRecord
+    POKEMON_TYPES = [nil, "Poison", "Flying", "Dragon", "Ground", "Fairy", "Grass", "Fighting", "Psychic", "Steel", "Ice", "Rock", "Dark", "Water", "Electric", "Fire", "Ghost", "Bug", "Normal"]
+
     validates :name, presence: true
     validates_uniqueness_of :name
 
-    validates :type_1, presence: true
+    validates :type_1, presence: true, :inclusion=> { :in => POKEMON_TYPES }
+    validates :type_2, :inclusion=> { :in => POKEMON_TYPES }
+    
     validates :hp, presence: true, numericality: { greater_than: 0 }
     validates :attack, presence: true, numericality: { greater_than: 0 }
     validates :defense, presence: true, numericality: { greater_than: 0 }
@@ -25,6 +29,12 @@ class Pokemon < ApplicationRecord
     validates :sp_defense, presence: true, numericality: { greater_than: 0 }
     validates :speed, presence: true, numericality: { greater_than: 0 }
     validates :generation, presence: true, numericality: { greater_than: 0 }
+
+    before_validation do
+        self.type_1 = self.type_1.downcase.capitalize
+        self.type_2 = self.type_2.downcase.capitalize if self.type_2.present?
+        self.type_2 = nil if self.type_2 == self.type_1
+    end
 
     before_save do
         self.legendary = false if self.legendary.nil?
--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class PokemonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    
+    def pokemon_data(name)
+        {name: name, type_1: "Water", hp: 5, attack: 5, defense: 5, sp_attack: 5, sp_defense: 5, speed: 5, generation: 1}
+    end
+    
+    test "legendary should be set to false if no specified" do
+        pokemon = Pokemon.create(pokemon_data("John Pokemon"))
+        assert pokemon.save
+        assert_not pokemon.legendary
+    end
+
+    test "stats total should be computed" do
+        pokemon = Pokemon.create(pokemon_data("Joe Pokemon"))
+        assert pokemon.save
+        assert pokemon.stats_total == 30
+    end
+
+    test "pokemon type should be lowercased then capitalized" do
+        data = pokemon_data("Barry")
+        data[:type_1] = "wAtEr"
+
+        pokemon = Pokemon.create(data)
+        assert pokemon.save
+        assert pokemon.type_1 == "Water"
+    end 
+    
+    test "pokemon type 2 should be removed if it equals type 1" do
+        data = pokemon_data("Barry")
+        data[:type_1] = "wAtEr"
+        data[:type_2] = "waTer"
+
+        pokemon = Pokemon.create(data)
+        assert pokemon.save
+        assert pokemon.type_1 == "Water"
+        assert pokemon.type_2.nil?
+    end 
+
 end
